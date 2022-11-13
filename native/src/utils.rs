25e9e4e4895ec3a38d34897332694e609f380731
Ovenoboyo <ovenoboyo@gmail.com>
2022-11-13 23:37:45 +0530

    Match event names and structs
    Signed-off-by: Ovenoboyo <ovenoboyo@gmail.com>

--- native/src/utils.rs
+++ native/src/utils.rs
@@ -14,7 +14,7 @@ where
             play_request_id,
             track_id,
         } => obj
-            .add_event("stopped")
+            .add_event("Stopped")
             .add_number("play_request_id", play_request_id)
             .add_spotify_id("track_id", track_id),
         PlayerEvent::Loading {
@@ -22,20 +22,20 @@ where
             track_id,
             position_ms,
         } => obj
-            .add_event("loading")
+            .add_event("Loading")
             .add_number("play_request_id", play_request_id)
             .add_spotify_id("track_id", track_id)
             .add_number("position_ms", position_ms as u64),
 
         PlayerEvent::Preloading { track_id } => obj
-            .add_event("preloading")
+            .add_event("Preloading")
             .add_spotify_id("track_id", track_id),
         PlayerEvent::Playing {
             play_request_id,
             track_id,
             position_ms,
         } => obj
-            .add_event("playing")
+            .add_event("Playing")
             .add_number("play_request_id", play_request_id)
             .add_spotify_id("track_id", track_id)
             .add_number("position_ms", position_ms as u64),
@@ -44,7 +44,7 @@ where
             track_id,
             position_ms,
         } => obj
-            .add_event("paused")
+            .add_event("Paused")
             .add_number("play_request_id", play_request_id)
             .add_spotify_id("track_id", track_id)
             .add_number("position_ms", position_ms as u64),
@@ -52,32 +52,32 @@ where
             play_request_id,
             track_id,
         } => obj
-            .add_event("timeToPreloadNextTrack")
+            .add_event("TimeToPreloadNextTrack")
             .add_number("play_request_id", play_request_id)
             .add_spotify_id("track_id", track_id),
         PlayerEvent::EndOfTrack {
             play_request_id,
             track_id,
         } => obj
-            .add_event("endOfTrack")
+            .add_event("EndOfTrack")
             .add_number("play_request_id", play_request_id)
             .add_spotify_id("track_id", track_id),
         PlayerEvent::Unavailable {
             play_request_id,
             track_id,
         } => obj
-            .add_event("unavailable")
+            .add_event("Unavailable")
             .add_number("play_request_id", play_request_id)
             .add_spotify_id("track_id", track_id),
         PlayerEvent::VolumeChanged { volume } => obj
-            .add_event("volumeChanged")
+            .add_event("VolumeChanged")
             .add_number("volume", volume as u64),
         PlayerEvent::PositionCorrection {
             play_request_id,
             track_id,
             position_ms,
         } => obj
-            .add_event("positionCorrection")
+            .add_event("PositionCorrection")
             .add_number("play_request_id", play_request_id)
             .add_spotify_id("track_id", track_id)
             .add_number("position_ms", position_ms as u64),
@@ -86,26 +86,26 @@ where
             track_id,
             position_ms,
         } => obj
-            .add_event("seeked")
+            .add_event("Seeked")
             .add_number("play_request_id", play_request_id)
             .add_spotify_id("track_id", track_id)
             .add_number("position_ms", position_ms as u64),
         PlayerEvent::TrackChanged { audio_item } => obj
-            .add_event("trackChanged")
+            .add_event("TrackChanged")
             .add_string("audio_item", audio_item.track_id.to_string()),
 
         PlayerEvent::SessionConnected {
             connection_id,
             user_name,
         } => obj
-            .add_event("sessionConnected")
+            .add_event("SessionConnected")
             .add_string("connection_id", connection_id)
             .add_string("user_name", user_name),
         PlayerEvent::SessionDisconnected {
             connection_id,
             user_name,
         } => obj
-            .add_event("sessionDisconnected")
+            .add_event("SessionDisconnected")
             .add_string("connection_id", connection_id)
             .add_string("user_name", user_name),
         PlayerEvent::SessionClientChanged {
@@ -114,22 +114,22 @@ where
             client_brand_name,
             client_model_name,
         } => obj
-            .add_event("sessionClientChanged")
+            .add_event("SessionClientChanged")
             .add_string("client_id", client_id)
             .add_string("client_name", client_name)
             .add_string("client_brand_name", client_brand_name)
             .add_string("client_model_name", client_model_name),
         PlayerEvent::ShuffleChanged { shuffle } => {
-            obj.add_event("shuffleChanged").add_bool("shuffle", shuffle)
+            obj.add_event("ShuffleChanged").add_bool("shuffle", shuffle)
         }
         PlayerEvent::RepeatChanged { repeat } => {
-            obj.add_event("repeatChanged").add_bool("repeat", repeat)
+            obj.add_event("RepeatChanged").add_bool("repeat", repeat)
         }
         PlayerEvent::AutoPlayChanged { auto_play } => obj
-            .add_event("autoPlayChanged")
+            .add_event("AutoPlayChanged")
             .add_bool("auto_play", auto_play),
         PlayerEvent::FilterExplicitContentChanged { filter } => obj
-            .add_event("filterExplicitContentChanged")
+            .add_event("FilterExplicitContentChanged")
             .add_bool("filter", filter),
     };
 
