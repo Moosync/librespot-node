51976ccd5b14e358e55e88e6acfa1668562d2297
Ovenoboyo <ovenoboyo@gmail.com>
2022-12-01 03:15:33 +0530

    Refresh dist
    Signed-off-by: Ovenoboyo <ovenoboyo@gmail.com>

--- dist/tokenHandler.d.ts
+++ dist/tokenHandler.d.ts
@@ -2,6 +2,7 @@ import { Token, TokenScope } from "./types";
 export declare class TokenHandler {
     private tokenMap;
     private filePath;
+    private readFilePromise;
     constructor(filePath: string);
     private dumpFile;
     private readFile;
