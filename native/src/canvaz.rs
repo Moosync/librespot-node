e54ecca66035f42e96cef31c1ba98060d752bed6
Sahil Gupte <ovenoboyo@gmail.com>
2023-10-23 20:45:24 -0400

    Rebuild dist

--- native/src/canvaz.rs
+++ native/src/canvaz.rs
@@ -23,7 +23,7 @@
 
 /// Generated files are compatible only with the same version
 /// of protobuf runtime.
-const _PROTOBUF_VERSION_CHECK: () = ::protobuf::VERSION_3_2_0;
+const _PROTOBUF_VERSION_CHECK: () = ::protobuf::VERSION_3_3_0;
 
 #[derive(PartialEq,Clone,Default,Debug)]
 // @@protoc_insertion_point(message:spotify.canvaz.cache.Artist)
@@ -936,6 +936,10 @@ impl ::protobuf::Enum for Type {
         Type::VIDEO_LOOPING_RANDOM,
         Type::GIF,
     ];
+
+    fn from_str(s: &str) -> Option<Self> {
+        Some(Self::VIDEO)
+    }
 }
 
 impl ::protobuf::EnumFull for Type {
